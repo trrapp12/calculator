7e72b379acb05dbd57ad624795fc741bc94c585a
trrapp@gmail.com <trrapp@gmail.com>
2016-10-11 19:24:27 -0600

    take out console.log

--- scripts/script.js
+++ scripts/script.js
@@ -13,48 +13,39 @@ function getUserInput () {
 getUserInput();
 
 function performOperation (number1, number2, operator) {
-    console.log("operator " + operator);
     if (operator === "+") {
       let answer = add (number1, number2);
-        console.log('add');
         return answer;
   }
     if (operator === "-") {
       let answer = subtract (number1, number2);
-        console.log('subtract');
         return answer;
   }
 
     if (operator === "*") {
       let answer = multiply (number1, number2);
-        console.log('multiply');
         return answer;
   }
 
     if (operator === "/") {
       let answer = divide (number1, number2);
-        console.log('divide');
         return answer;
     }
 }
 
 
 function add (number1, number2) {
-  console.log('fun add');
   return (number1 + number2);
 }
 
 function subtract (number1, number2) {
-  console.log('fun subtract');
   return (number1 - number2);
 }
 
 function multiply (number1, number2) {
-  console.log('fun multiply');
   return (number1 * number2);
 }
 
 function divide (number1, number2) {
-  console.log('fun divide');
   return (number1 / number2);
   }
