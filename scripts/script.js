83ff6f27fedc7f29d2240cf5f9d04e22bbff0d16
Trevor Rapp <trrapp@gmail.com>
2021-08-20 21:43:04 -0600

    fixes wording in prompt boxes

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,55 @@
+(function () {
+  function getUserInput () {
+      let number1 = Number( prompt("what is your first number?"));
 
+      let operator = prompt("What operation do you want to perform?  Type \"+\"  for add,  \"-\"  for subtract,  \"*\"  for multiplication,  or \"/\"  for division. ");
 
-function getUserInput () {
-    let number1 = Number( prompt("what is your first number?"));
+      let number2 = Number( prompt("What is your second number?"));
 
-    let operator = prompt("What operation do you want to perform?");
-
-    let number2 = Number( prompt("What is your second number?"));
-
-    return performOperation (number1, number2, operator);
-  }
-
-getUserInput();
-
-function performOperation (number1, number2, operator) {
-    if (operator === "+") {
-      let answer = add (number1, number2);
-        return answer;
-  }
-    if (operator === "-") {
-      let answer = subtract (number1, number2);
-        return answer;
-  }
-
-    if (operator === "*") {
-      let answer = multiply (number1, number2);
-        return answer;
-  }
-
-    if (operator === "/") {
-      let answer = divide (number1, number2);
-        return answer;
+      return performOperation (number1, number2, operator);
     }
-}
 
+  getUserInput();
 
-function add (number1, number2) {
-  return (number1 + number2);
-}
+  function performOperation (number1, number2, operator) {
+      if (operator === "+") {
+        let answer = add (number1, number2);
+          alert("your answer is: " + answer)
+          return answer;
+    }
+      if (operator === "-") {
+        let answer = subtract (number1, number2);
+          alert("your answer is: " + answer)
+          return answer;
+    }
 
-function subtract (number1, number2) {
-  return (number1 - number2);
-}
+      if (operator === "*") {
+        let answer = multiply (number1, number2);
+          alert("your answer is: " + answer)
+          return answer;
+    }
 
-function multiply (number1, number2) {
-  return (number1 * number2);
-}
-
-function divide (number1, number2) {
-  return (number1 / number2);
+      if (operator === "/") {
+        let answer = divide (number1, number2);
+          alert("your answer is: " + answer)
+          return answer;
+      }
   }
+
+
+  function add (number1, number2) {
+    return (number1 + number2);
+  }
+
+  function subtract (number1, number2) {
+    return (number1 - number2);
+  }
+
+  function multiply (number1, number2) {
+    return (number1 * number2);
+  }
+
+  function divide (number1, number2) {
+    return (number1 / number2);
+    }
+})();
